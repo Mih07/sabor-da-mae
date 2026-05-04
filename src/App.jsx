2f03f35439361456a6e93603501907a012aefe0c
Michele Santo <micheleespsanto@gmail.com>
2026-05-04 18:13:07 -0300

    ajuste

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   // --- ESTADOS ---
